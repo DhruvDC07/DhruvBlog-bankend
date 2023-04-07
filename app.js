e9a4f71e59492667ba4fc4ebe8df23c3725dbdd8
Dhruv kumar <108050502+Dhruv-Kumar-au49@users.noreply.github.com>
2023-04-07 20:12:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
-const whitelist = ['http://localhost:3000']; // add the origin of your frontend app here
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
 const { urlencoded, json } = require('express');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
