5d1bf0897df68f0c29caec3f582e5a0ca8d08237
Dhruv kumar <108050502+Dhruv-Kumar-au49@users.noreply.github.com>
2023-04-07 20:32:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const { urlencoded, json } = require('express');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
+const { initDB } = require('./mongoDB.js');
+const { login, signup, logout } = require('./controller/user');
+const {
+  getallblog,
+  addblog,
+  updateblog,
+  deletblog,
+  getById,
+  getByUserId,
+} = require('./controller/blog');
+
+// Load environment variables
+dotenv.config();
+
+// Set up body parser and cookie parser middleware
+app.use(urlencoded({ extended: true }));
+app.use(json());
+app.use(cookieParser());
+
+// Initialize the database connection
+initDB();
+
+// Set up routes for authentication
+app.post('/signup', signup);
+app.post('/login', login);
+app.post('/logout', logout);
+
+// Set up routes for blog management
+app.get('/', getallblog);
+app.post('/add', addblog); 
+app.put('/update/:id', updateblog);
+app.get('/:id', getById);
+app.delete('/:id', deletblog);
+app.get('/user/:id', getByUserId);
+
+// Start the server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
